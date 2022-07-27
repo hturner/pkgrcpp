c636a5e4219dc797e29c95f45d5a855d2e15df69
Heather Turner <ht@heatherturner.net>
2022-07-27 08:05:52 +0100

    export R and C interface

--- src/add_cpp2.cpp
+++ src/add_cpp2.cpp
@@ -1,6 +1,13 @@
 #include <Rcpp.h>
 using namespace Rcpp;
+// [[Rcpp::interfaces(r, cpp)]]
 
+//' Add Two Numbers
+//'
+//' Return the sum of two numbers.
+//' @param x
+//' @param y
+//' @export
 // [[Rcpp::export]]
 double add_cpp2(double x, double y) {
   double value = x + y;
